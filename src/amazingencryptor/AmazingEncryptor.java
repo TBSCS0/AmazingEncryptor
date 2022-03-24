b31eb9dbc0ddeb3a5c17f6b4199cefc164676838
oscarneiva <oscarneivaeneto@gmail.com>
2022-03-24 15:24:16 -0300

    Initial commit

---
+++ src/amazingencryptor/AmazingEncryptor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amazingencryptor;
+
+/**
+ *
+ * @author Oscar Neiva
+ */
+public class AmazingEncryptor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

c3dab4d9402d6bd002e4ebcfca2287ed55a983c7
LuizEduardoRaffaini <luiz.raffaini@gmail.com>
2022-04-26 11:39:42 -0300

    optimisation of code

--- src/amazingencryptor/TestingArea.java
+++ src/amazingencryptor/TestingArea.java
@@ -1,39 +1,30 @@
 package amazingencryptor;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class TestingArea {
-    public static void main(String args[]) throws NoSuchAlgorithmException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        List<Character> charList = new ArrayList<Character>();
-        for(char code = 0; code < 256; code++) {
-            charList.add(code);
-        }
-        char[] ascii = new char[charList.size()];
-        for (int i = 0; i < ascii.length; i++) {
-            ascii[i] = charList.get(i);
-        }
-        //Inputing string
+        //Creation of Ascii Characters Array:
+        String s = new String(IntStream.rangeClosed(32, 126).toArray(), 0, 95);
+        char[] ascii = s.toCharArray();
+        //Inputting string:
         System.out.println("Enter you password below:");
         String passString = scanner.nextLine();
-        //creating brute force
+        //Creating brute force:
         char[] passGuess = new char[passString.length()];
-        char[] passChar = new char[passString.length()];
-        for (int i = 0; i < passString.length(); i++) {
-            passChar[i] = passString.charAt(i);
-        }
+        char[] passChar = passString.toCharArray();
         for (int i = 0; i < passString.length(); i++) {
             System.out.println(passGuess);
-            for (int j = 0; j < charList.size(); j++) {
+            for (int j = 0; j < ascii.length; j++) {
                 if (passChar[i] == ascii[j]) {
                     passGuess[i] = passChar[i];
-                    j = charList.size();
+                    j = ascii.length;
                 }
             }
         }
         System.out.println(passGuess);
-        System.out.println(new StringBuilder().append("I guess your password is: ").append(passGuess).toString());
+        System.out.println("I guess your password is: " + String.valueOf(passGuess));
     }
 }
